e4abad98583574f57e77ac38f761ccd3a68e3046
Davoleo <davoleo2001@gmail.com>
2021-02-24 20:15:37 +0100

    Vertex Buffers | UIDs
    - GPU drawing as a state machine

--- src/application.cpp
+++ src/application.cpp
@@ -33,6 +33,34 @@ int main()
     //Print the supported OpenGL version
     std::cout << glGetString(GL_VERSION) << std::endl;
 
+    //Concepts
+    //To draw a triangle in modern OpenGL we'll need 2 things
+    //1) a Vertex Buffer (a buffer used to store bytes in memory (although in the VRAM))
+    //2) a Shader is a program that runs on the GPU
+
+    //You select a buffer and a shader which are based on the status of the GPU and then the GPU draws the triangle depending on those two things
+    // **State Machine**
+
+    //Defining the buffer outside the frame loop
+    float positions[6] = {
+            -0.5, -0.5,
+            0, 0.5,
+            0.5, -0.5
+    };
+
+    //Will be populated with the UID of the generated object in the GPU VRAM
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    //Specifies the meaning of the buffer (in this case just a buffer of memory (array))
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    //Size is in bytes | you can place NULL in place of the third parameter in case you don't want to provide initial values for the buffer
+    //GL_STATIC_DRAW is the mode that this buffer will be optimized for (can be STATIC or DYNAMIC data)
+    //Docs: http://docs.gl/gl4/glBufferData
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    //Will reset the buffer to be bound to nothing
+    //glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -40,11 +68,18 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
 
         //Legacy OpenGL Way to create a triangle
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5, -0.5);
-        glVertex2f(0, 0.5);
-        glVertex2f(0.5, -0.5);
-        glEnd();
+        //glBegin(GL_TRIANGLES);
+        //glVertex2f(-0.5, -0.5);
+        //glVertex2f(0, 0.5);
+        //glVertex2f(0.5, -0.5);
+        //glEnd();
+
+        //Draw things when you don't have an index buffer
+        //First parameter: the primitive you want to render
+        //Second Parameter: the offset from the first item of the data buffer
+        //Third Parameter: the number of indices (vertices) you want to render
+        //It will draw depending ON THE BOUND buffer (you don't need to pass the buffer since openGL works as a state machine)
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
